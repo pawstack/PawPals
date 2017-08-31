fe400c1afdd3540265f2718fe424d3a993847b33
mkchang <martinkchang@gmail.com>
2017-08-31 12:17:14 -0700

    Add signup placeholder routing and component

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Registration from './components/Registration';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import SignUp from './components/SignUp';
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World from React</h1>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to='/signup'>Signup</Link></li>
+          </ul>
+        
+        <Route exact path="/" render={() => (
+          <h1>Hello World from React</h1>
+        )}/>
+        <Route path="/signup" component={SignUp}/>
+        </div>
+      </Router>
     );
   }
 }
