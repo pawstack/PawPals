363ca1b620f24b00ac49c351315f2a0daf53191a
novyQ <novaqiu@gmail.com>
2017-09-04 17:19:22 -0700

    add phone info to database

--- client/src/components/SignUpDataEntry.js
+++ client/src/components/SignUpDataEntry.js
@@ -8,20 +8,21 @@ import {
   Redirect
 } from 'react-router-dom';
 
+
 class SignUpDataEntry extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      phone: ''
-    };
   }
 
   updatePhone(e) {
-    this.setState({phone: e.target.value}),
-    ()=>{
-      this.props.handlePhoneUpdate( this );
-    };
+    const phone = e.target.value;
+    this.props.entriesChanged( this, 'phone', phone );
+  }
+
+  updateAddress(e) {
+    const address = e.target.value;
+    this.props.entriesChanged( this, 'address', address );
   }
 
   render() {
@@ -30,7 +31,7 @@ class SignUpDataEntry extends React.Component {
         <div>
           <div>Step 1</div>
           <div>Phone # <input onChange = {this.updatePhone.bind(this)}></input></div>
-          <div>Address <input></input></div>
+          <div>Address <input onChange = {this.updateAddress.bind(this)}></input></div>
         </div>
       </Router>
     );
--- client/src/components/SignUpStart.js
+++ client/src/components/SignUpStart.js
@@ -17,21 +17,39 @@ class SignUpStart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      phone:""
+      phone: '',
+      address: ''
     };
+  }
+
+
+  handleEntriesChanged( component, valueType, value ) {
+    this.setState( { [valueType]: value } );
   }
 
   render() {
     return (
       <Router>
         <div>
-          <div><Link to = "/signup/owner">Owner</Link></div>
-          <div><Link to = "/signup/walker">Walker</Link></div>
+          <div>
+            <Link to = "/signup/owner">Owner</Link>
+          </div>
+          <div>
+            <Link to = "/signup/walker">Walker</Link>
+          </div>
+
           <Switch>
-            <Route exact path="/signup/start" component={SignUpDataEntry}/>
-            <Route exact path="/signup/owner" component={OwnerRegister}/>
-            <Route exact path="/signup/walker" component={WalkerRegister}/>
+            <Route exact path = "/signup/start" render = {()=>
+              <SignUpDataEntry entriesChanged = {this.handleEntriesChanged.bind(this)}/>
+            }/>
+            <Route exact path = "/signup/owner" render = {()=>
+              <OwnerRegister phoneInfo={this.state.phone}/>
+            }/>
+            <Route exact path = "/signup/walker" render = {()=>
+              <WalkerRegister phoneInfo={this.state.phone}/>
+            }/>
           </Switch>
+
         </div>
       </Router>
     );
