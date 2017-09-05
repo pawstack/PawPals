3b366d4bfda0e45b3e8ffc6a0cab761612b67881
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-05 15:44:15 -0700

    updated /tokenize to save stripe user id

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const middleware = require('../middleware');
 var curl = require('curlrequest');
+var controllers = require('../controllers');
 
 const router = express.Router();
 
@@ -8,8 +9,9 @@ router.route('/')
   .get(middleware.auth.verify, (req, res) => {
     console.log('the req query is ', req.query);
     if (req.query.code) {
+      console.log('*****the user ID from passport is includes ', req.user.id);
       console.log('****** retrieved the temporary token.  will invoke step4 to finalize the stripe_user_id');
-      res.redirect('/tokenize/?code=' + req.query.code);
+      res.redirect('/tokenize/?code=' + req.query.code + '&userid=' + req.user.id);
     }
     res.render('index.ejs');
   });
@@ -26,30 +28,26 @@ router.route('/login')
 
 router.route('/tokenize')
   .get((req, res) => {
-    debugger;
-    // req.flash('client_secret', 'sk_test_2U8Pw3mVMwmqXjdSHbiBfFSm');
-    // req.flash('code', 'ac_BKpjN5v1m4YCxeoG2ompHv9qbNILoLLc');
-    // req.flash('grant_type', 'authorization_code');
-    //
-    // res.redirect('https://connect.stripe.com/oauth/token');
-
     curl.request({
       url: 'https://connect.stripe.com/oauth/token',
       data: {
         'client_secret': 'sk_test_2slmpAIrhlZSnWP7KSMNp6HX', // need to change this periodically
-        'code': req.query.code,
-        //'code': 'ac_BKskFL5z0e7rB4OySIe0EKWVFiD1wFVw', // will need to pull this in from the query data of the ajax get request.
+        'code': req.query.code, //'ac_BKskFL5z0e7rB4OySIe0EKWVFiD1wFVw', pull this in from the query data of the ajax get request.
         'grant_type': 'authorization_code'
       }
     }, function(err, stdout, meta) {
       console.log('curl request completed...');
-      console.log('err ', err);
       console.log('stdout ', stdout);
-      console.log('the type of stdout is ', typeof stdout);
-      console.log('meta ', meta);
-      console.log('the stripe users id that should be saved to the db is ', JSON.parse(stdout).stripe_user_id);
-      res.send(JSON.parse(stdout).stripe_user_id);
+      var stripe_user_id = JSON.parse(stdout).stripe_user_id;
+      controllers.Profiles.saveStripeUserID(req.query.userid, stripe_user_id)
+        .then(() => {
+          console.log('redirecting to api/payment');
+          res.redirect('/api/payment'); //redirect to the page where the user will input their CC info.
+        });
+
+      //res.send(JSON.parse(stdout).stripe_user_id);
       //now save the stripe_user_id in the database under the user!
+      //res.redirect('/api/payment');//redirect to the page where the user will input their CC info.
       //create a customer object - Customer objects allow you to perform recurring charges and
       //  track multiple charges that are associated with the same customer.
       //  The API allows you to create, delete, and update your customers.
@@ -58,6 +56,14 @@ router.route('/tokenize')
       //res.send(200);
     });
   });
+
+
+// router.route('/stripe')
+//   .get((req, res) => {
+//     console.log('in the /stripe route');
+//     res.redirect('http://google.com');
+//     //res.redirect('https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://localhost:3000/&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123');
+//   });
 
 // new users/signups also go through /login, registration happens after if they are a new user
 // router.route('/signup')
