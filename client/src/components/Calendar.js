e1fcd3e8fa15ff28353904fb9eed8def150a370a
Young <young@severus.local>
2017-09-05 15:26:53 -0700

    added dialog window to examine existing walks

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -3,7 +3,8 @@ import { render } from 'react-dom'
 import moment from 'moment'
 import BigCalendar from 'react-big-calendar'
 import DialogForm from './DialogForm'
-
+import EventDialog from './EventDialog'
+  
 BigCalendar.momentLocalizer(moment)
 require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
 
@@ -12,24 +13,41 @@ class Calendar extends React.Component {
     super(props)
     this.state = {
       events: [],
-      open: false,
+      formOpen: false,
+      eventOpen: false,
       start: null,
       end: null,
       price: 20,
-      location: 'default'
+      location: 'default',
+      selectedEvent: {
+        walk_zone_pt: null,
+        start: null,
+        end: null
+      }
     };
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.handleFormOpen =  this.handleFormOpen.bind(this);
+    this.handleFormClose = this.handleFormClose.bind(this);
     this.handleTextInputChange = this.handleTextInputChange.bind(this);
+    this.handleEventOpen = this.handleEventOpen.bind(this);
+    this.handleEventClose = this.handleEventClose.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
   }
 
-  handleOpen(start, end) {
-    this.setState({open: true, start, end});
+  handleEventOpen() {
+    this.setState({eventOpen: true});
+  };
+
+  handleEventClose() {
+    this.setState({eventOpen: false});
+  };
+
+  handleFormOpen(start, end) {
+    this.setState({formOpen: true, start, end});
   }
 
-  handleClose() {
-    this.setState({open: false});
+  handleFormClose() {
+    this.setState({formOpen: false});
   }
 
   handleTextInputChange(key, value) {
@@ -44,20 +62,28 @@ class Calendar extends React.Component {
       .then((response) => response.json())
       .then((data) => {
         var events = [];
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.walks.length; i++) {
           var event = {'title': 'walk'};
-          event['start'] = new Date(data[i].session_start);
-          event['end'] = new Date(data[i].session_end);
-          event['price'] = data[i].price;
-          event['neighbourhood'] = data[i].walk_zone_pt;
+          event['start'] = new Date(data.walks[i].session_start);
+          event['end'] = new Date(data.walks[i].session_end);
+          event['price'] = data.walks[i].price;
+          event['neighbourhood'] = data.walks[i].walk_zone_pt;
           events.push(event);
         }
+        var states = {
+          events: events,
+          location: data.location
+        };
         callback(events);
       })
       .catch((err) => {
         console.log('error:',err)
       })
   }
+
+  handleCancel() {
+    console.log('event should cancel')
+  };
 
   handleSubmit(event) {
     event.preventDefault();
@@ -92,18 +118,19 @@ class Calendar extends React.Component {
   render () {
     return (
       <div>
-        <DialogForm handleTextInputChange={this.handleTextInputChange} profile_id={this.state.profile_id} open={this.state.open} handleClose={this.handleClose} handleOpen={this.handleOpen} handleSubmit={this.handleSubmit} start={this.state.start} end={this.state.end} price={this.state.price} location={this.state.location}/>
         <BigCalendar
           selectable
           events={this.state.events}
           defaultView='week'
           scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={new Date()}
-          onSelectEvent={event => console.log(event)}
+          onSelectEvent={event => {this.setState({selectedEvent: event, eventOpen: true})}}
           onSelectSlot={(slotInfo) => {
-            this.handleOpen(slotInfo.start, slotInfo.end);
+            this.handleFormOpen(slotInfo.start, slotInfo.end);
           }}
         />
+        <DialogForm handleTextInputChange={this.handleTextInputChange} open={this.state.formOpen} handleClose={this.handleFormClose} handleOpen={this.handleFormOpen} handleSubmit={this.handleSubmit} start={this.state.start} end={this.state.end} price={this.state.price} location={this.state.location}/>
+        < EventDialog handleCancel={this.handleCancel} open={this.state.eventOpen} handleClose={this.handleEventClose} handleOpen={this.handleEventOpen} selectedEvent={this.state.selectedEvent}/>
       </div>
     )
   }
---
+++ client/src/components/EventDialog.js
@@ -0,0 +1,69 @@
+import Dialog from 'material-ui/Dialog';
+import React from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import {orange500, blue500} from 'material-ui/styles/colors';
+import Checkbox from 'material-ui/Checkbox';
+import moment from 'moment'
+
+const styles = {
+  radioButton: {
+    marginTop: 16,
+    errorStyle: {
+      color: orange500,
+    },
+    underlineStyle: {
+      borderColor: orange500,
+    },
+    floatingLabelStyle: {
+      color: orange500,
+    },
+    floatingLabelFocusStyle: {
+      color: blue500,
+    },
+  }
+}
+
+export default class EventDialog extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel Walk"
+        primary={false}
+        onClick={this.props.handleCancel}
+      />,
+      <FlatButton
+        label="Close"
+        primary={true}
+        keyboardFocused={true}
+        onClick={this.props.handleClose}
+      />,
+    ];
+    const start = moment(this.props.selectedEvent.start, 'YYYY-YY-DD hh:mm:ss').format('llll');
+    const end = moment(this.props.selectedEvent.end, 'YYYY-YY-DD hh:mm:ss').format('LT');
+    return (
+      <div>
+        <RaisedButton label="Dialog" onClick={this.props.handleOpen} />
+        <Dialog
+          title="Walk Details"
+          actions={actions}
+          modal={false}
+          open={this.props.open}
+          onRequestClose={this.props.handleClose}
+        >
+          <ul>
+            <li> Area: {this.props.selectedEvent.neighbourhood}</li>
+            <li> Attendees: Currently not a feature </li>
+            <li> Price: ${this.props.selectedEvent.price} </li>
+            <li> Time: {start} to {end} </li>
+          </ul>
+      </Dialog>
+      </div>
+    );
+  }
+}
