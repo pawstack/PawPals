19ef5fda6aac1ce1f513270aedd3d35fdba0e523
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-05 16:56:29 -0700

    refactor server/app.js and moved routing to /api

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const path = require('path');
 const middleware = require('./middleware');
 const routes = require('./routes');
-
-//const stripe = require('./stripeAPI.js');
+const cors = require('cors');
 var stripe = require('stripe')('sk_test_2slmpAIrhlZSnWP7KSMNp6HX');
 
 
 const app = express();
 const database = require('../db/index');
+
+
 
 app.use(middleware.morgan('dev'));
 app.use(middleware.cookieParser());
@@ -23,12 +24,18 @@ app.use(middleware.passport.initialize());
 app.use(middleware.passport.session());
 app.use(middleware.flash());
 
-app.use(express.static(path.join(__dirname, '../public')));
 
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested With, Content-Type, Accept');
+  next();
+});
 app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
-app.use('/api/payment', routes.payment);
 app.use('/*', routes.auth);
 
 module.exports = app;
