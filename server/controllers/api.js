329c14454884d6233a2b437b4dec7048dbd6ba8d
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-05 19:12:21 -0700

    refactor code from routes into the controllers

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,6 +1,9 @@
 const models = require('../../db/models');
 const knex = require('knex')(require('../../knexfile'));
 const db = require('bookshelf')(knex);
+var curl = require('curlrequest');
+var stripe = require('stripe')('sk_test_2slmpAIrhlZSnWP7KSMNp6HX');
+var controllers = require('./');
 
 db.plugin('registry');
 
@@ -155,3 +158,50 @@ module.exports.saveWalker = function(req, res) {
     .then(res.send(200));
 };
 
+module.exports.getAndSaveStripeID = (req, res) => {
+  console.log('*********** inside of controller of /signup/stripeid');
+  curl.request({
+    url: 'https://connect.stripe.com/oauth/token',
+    data: {
+      'client_secret': 'sk_test_2slmpAIrhlZSnWP7KSMNp6HX', // need to change this periodically
+      'code': req.query.code, //'ac_BKskFL5z0e7rB4OySIe0EKWVFiD1wFVw', pull this in from the query data of the ajax get request.
+      'grant_type': 'authorization_code'
+    }
+  }, function(err, stdout, meta) {
+    console.log('curl request completed...');
+    console.log('stdout ', stdout);
+    var stripe_user_id = JSON.parse(stdout).stripe_user_id;
+    controllers.Profiles.saveStripeUserID(req.query.userid, stripe_user_id)
+      .then(() => {
+        console.log('redirecting to api/payment');
+        res.redirect('/api/signup/payment/tokenizecard'); //redirect to the page where the user will input their CC info.
+      });
+
+    //res.send(JSON.parse(stdout).stripe_user_id);
+    //now save the stripe_user_id in the database under the user!
+    //res.redirect('/api/payment');//redirect to the page where the user will input their CC info.
+    //create a customer object - Customer objects allow you to perform recurring charges and
+    //  track multiple charges that are associated with the same customer.
+    //  The API allows you to create, delete, and update your customers.
+    //  You can retrieve individual customers as well as a list of all your customers.
+    //then redirect back to the home page?  res.redirect('/home');
+    //res.send(200);
+  });
+};
+
+module.exports.getAndsaveCardToken = (req, res) => {
+  console.log('******* INSIDE OF CONTORLLER SIGNUP/PAYMENT/SAVECARDTOKEN');
+  console.log('the req body is ', req.body);
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken //stripe checkout creates a token for the CC
+  })
+    .then((customer) => {
+      console.log('the (tokenized) customer id for this new card is ', customer.id);
+      var tokenizedCard = customer.id;
+      controllers.Profiles.saveTokenizedCC(req.user.email, tokenizedCard) //SAVE THE CUSTOMER ID TO THE DB! userid, token
+        .then(() => {
+          res.redirect('/');
+        });
+    });
+};
