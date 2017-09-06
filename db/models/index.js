fb4ff9c167d09f478a1e1117a157934fdca7d897
Young <young@severus.local>
2017-09-06 14:48:54 -0700

    updated bookshelf models to reflect relations

--- db/models/index.js
+++ db/models/index.js
@@ -2,3 +2,4 @@ module.exports.Auth = require('./auths');
 module.exports.Profile = require('./profiles');
 module.exports.Dog = require('./dogs');
 module.exports.Walk = require('./walks');
+module.exports.Dog = require('./dogs');
--- db/models/walks.js
+++ db/models/walks.js
@@ -2,12 +2,16 @@ const db = require('../');
 
 const Walk = db.Model.extend({
   tableName: 'walks',
-  profiles: function() {
-    return this.belongsToMany('Profile');
+  walker: function() {
+    return this.belongsTo('Profile', 'walker_id')
+  },
+  owner: function() {
+    return this.belongsTo('Profile', 'owner_id')
   },
   dog: function() {
-    return this.belongsTo('Dog');
+    return this.belongsTo('Dog', 'dog_id')
   }
 });
 
 module.exports = db.model('Walk', Walk);
+
