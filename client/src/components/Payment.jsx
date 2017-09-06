c12eb1c7174501e81f097677b0e53ceaf7bd55ad
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-05 17:20:24 -0700

    in work - payment.jsx

--- client/src/components/Payment.jsx
+++ client/src/components/Payment.jsx
@@ -6,6 +6,10 @@ import {
   Link
 } from 'react-router-dom';
 import $ from 'jquery';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
 
 class Payment extends React.Component {
 
@@ -17,6 +21,7 @@ class Payment extends React.Component {
     this.handleTestPayClick = this.handleTestPayClick.bind(this);
     this.createDestinationCharge = this.createDestinationCharge.bind(this);
     this.requestRefundForCancellation = this.requestRefundForCancellation.bind(this);
+    this.handleStripeRegistration = this.handleStripeRegistration.bind(this);
   }
 
 
@@ -102,37 +107,91 @@ class Payment extends React.Component {
     });
   }
 
+  handleStripeRegistration() {
+    console.log('button clicked to handle stripe registration');
+    $.ajax({
+      type: 'POST',
+      url: '/stripe',
+      data: {
+        redirect_uri: 'http://localhost:3000/',
+        client_id: 'ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE',
+        state: 'test123'
+      },
+      success: () => {
+        console.log('client - successfully registered with stripe');
+      },
+      error: (error) => {
+        console.log('client - error in registering with stripe', error);
+      }
+    });
+  }
+
 
 
 
   render() {
-
-
     return (
       <div>
+        <MuiThemeProvider>
+          <div>
+            <div>
+              <RaisedButton onClick = {this.handleStripeRegistration} label="Connect with Stripe - AJAX" primary={true}/>
+            </div>
+            <div>
+              <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://localhost:3000/&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123'>
+                <RaisedButton label="Connect with Stripe" primary={true}/>
+              </a>
+            </div>
+            <div>
+              <RaisedButton onClick = {this.createDestinationCharge} label="Create a Destination Charge" primary ={true}/>
+            </div>
+            <div>
+              <RaisedButton onClick = {this.requestRefundForCancellation} label = "Request Refund for Cancellation" primary = {true}/>
+            </div>
+          </div>
+        </MuiThemeProvider>
         <h1>Payment</h1>
-
-        <button onClick = {this.handleTestPayClick}>connect with stripe</button>
         <div>
           <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://stripe.com/connect/default/oauth/test&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123'>TRY 9 - Default Redirect</a>
         </div>
-        <div>
-          <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://localhost:3000/&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123'>TRY 10 - back to local host (STEP 1-4)</a>
-        </div>
-        <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://google.com&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=tiffany'>BUTTON 2</a>
-        <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=https://google.com'>BUTTON 3</a>
-        <div>
-          <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=http://localhost:3000/'>CREATE A NEW ACCOUNT</a>
-
-        </div>
-        <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=http://localhost:3000/&stripe_user[email]=test@gmail.com'>BUTTON 5</a>
-
-        <div><button onClick = {this.createDestinationCharge}>Create a Destination Charge</button></div>
-        <div><button onClick = {this.requestRefundForCancellation}>Request Refund for Cancellation</button></div>
-
+        <button onClick = {this.handleTestPayClick}>connect with stripe</button>
       </div>
     );
   }
 }
 
 export default Payment;
+
+
+
+/*
+
+<div>
+  <MuiThemeProvider>
+    <RaisedButton label="Primary" primary={true}/>
+  </MuiThemeProvider>
+  <h1>Payment</h1>
+
+  <button onClick = {this.handleTestPayClick}>connect with stripe</button>
+  <div>
+    <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://stripe.com/connect/default/oauth/test&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123'>TRY 9 - Default Redirect</a>
+  </div>
+  <div>
+    <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://localhost:3000/&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=testing123'>TRY 10 - back to local host (STEP 1-4)</a>
+  </div>
+  <a href = 'https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://google.com&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&state=tiffany'>BUTTON 2</a>
+  <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=https://google.com'>BUTTON 3</a>
+  <div>
+    <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=http://localhost:3000/'>CREATE A NEW ACCOUNT</a>
+
+  </div>
+  <a href = 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_BKKqX6IKWv2zjuHsLKdReiYfTfnaNPIE&scope=read_write&redirect_uri=http://localhost:3000/&stripe_user[email]=test@gmail.com'>BUTTON 5</a>
+
+  <div><button onClick = {this.createDestinationCharge}>Create a Destination Charge</button></div>
+  <div><button onClick = {this.requestRefundForCancellation}>Request Refund for Cancellation</button></div>
+
+
+
+</div>
+
+*/
