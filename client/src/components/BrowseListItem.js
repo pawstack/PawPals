a61175d0cd2c618a98b2b2be4fbfa68b2255f0a4
Young <young@severus.local>
2017-09-07 17:24:35 -0700

    edited date on browse walks

--- client/src/components/BrowseListItem.js
+++ client/src/components/BrowseListItem.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import moment from 'moment';
 
 class BrowseListItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
     };
+    this.convertDate = this.convertDate.bind(this);
+  }
+
+  convertDate(start, end) {
+    let jsStart= new Date(start);
+    let jsEnd= new Date(end);
+    return [moment(jsStart, 'YYY-MM-DD hh:mm:ss').format('llll'), moment(jsEnd, 'YYYY-MM-DD hh:mm:ss').format('LT')];
   }
 
   render() {
+    let time = this.convertDate(this.props.walk.session_start, this.props.walk.session_end);
     return (
       <Card>
         <CardHeader
@@ -17,7 +26,7 @@ class BrowseListItem extends React.Component {
           subtitle={`Rating: ${this.props.walk.walker.avg_walker_rating}`}
           avatar={this.props.walk.walker.profile_pic}
         />
-        <CardTitle title={`$${this.props.walk.price}/hour`} subtitle={`Start: ${new Date(this.props.walk.session_start)}, Stop: ${new Date(this.props.walk.session_end)}, Near: ${this.props.walk.walk_zone_pt}`} />
+        <CardTitle title={`$${this.props.walk.price}/hour`} subtitle={`From ${time[0]} to ${time[1]}, Near: ${this.props.walk.walk_zone_pt}`} />
         <CardText>
           {this.props.walk.walker.about_me}
         </CardText>
@@ -32,5 +41,7 @@ class BrowseListItem extends React.Component {
     );
   }
 }
+
+
 
 export default BrowseListItem;
