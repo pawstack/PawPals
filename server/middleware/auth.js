6cbadfd61bce9872330ba8a785c31ef75e00ee81
mkchang <martinkchang@gmail.com>
2017-09-13 19:49:34 -0700

    Update redis sessions auth to use config vars

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,21 @@
+const redisConfig = require('config')['redis'];
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
+
+if (process.env.NODE_ENV === 'production') {
+  const redisClient = require('redis').createClient(redisConfig.url);
+  const redisStoreOptions = {
+    client: redisClient,
+    url: redisConfig.url
+  };
+} else {
+  const redisClient = require('redis').createClient();
+  const redisStoreOptions = {
+    client: redisClient,
+    host: redisConfig.host,
+    port: redisConfig.port
+  };
+}
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -10,12 +25,8 @@ module.exports.verify = (req, res, next) => {
 };
 
 module.exports.session = session({
-  store: new RedisStore(process.env.REDIS_URL || {
-    client: redisClient,
-    host: 'localhost',
-    port: 6379
-  }),
-  secret: 'more laughter, more love, more life',
+  store: new RedisStore(redisStoreOptions),
+  secret: redisConfig.secret,
   resave: false,
   saveUninitialized: false
 });
