e7bdab44914ec748ce80edd925236cf94b853669
mkchang <martinkchang@gmail.com>
2017-09-13 20:35:32 -0700

    update config access to redis_url

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,19 +1,20 @@
 const redisConfig = require('config')['redis'];
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const redisClient = require('redis').createClient(redisConfig.url);
 
-if (process.env.NODE_ENV === 'production') {
-  const store = new RedisStore({
-    url: redisConfig.url
-  });
-} else {
-  const redisClient = require('redis').createClient();
-  const store = new RedisStore({
-    client: redisClient,
-    host: redisConfig.host,
-    port: redisConfig.port
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   const store = new RedisStore({
+//     url: redisConfig.url
+//   });
+// } else {
+//   const redisClient = require('redis').createClient();
+//   const store = new RedisStore({
+//     client: redisClient,
+//     host: redisConfig.host,
+//     port: redisConfig.port
+//   });
+// }
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -23,7 +24,12 @@ module.exports.verify = (req, res, next) => {
 };
 
 module.exports.session = session({
-  store: store,
+  store: new RedisStore({
+    client: redisClient,
+    host: redisConfig.host,
+    port: redisConfig.port,
+    url: redisConfig.url
+  }),
   secret: redisConfig.secret,
   resave: false,
   saveUninitialized: false
