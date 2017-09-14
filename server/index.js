6fe12230bb3dd1e6fc791d4077c982ca58fba56c
mkchang <martinkchang@gmail.com>
2017-09-14 14:48:59 -0700

    Update process.env call to all caps PORT

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 'use strict';
 const server = require('./app');
 const db = require('../db');
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
-
-server.listen(PORT, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(PORT, () => {
+  console.log('Example app listening on port ' + PORT + '!');
 });
