93b06f390599010ebd9bcd840c919eee29ec31d1
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-13 19:34:16 -0700

    change google map marker to polyline

--- client/src/components/FindMyDogMap.js
+++ client/src/components/FindMyDogMap.js
@@ -4,7 +4,36 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  Circle,
+  Polyline
 } from 'react-google-maps';
+
+// const GoogleMapWrapper = withGoogleMap(props => (
+//   <GoogleMap
+//     ref={props.onMapLoad}
+//     defaultZoom={15}
+//     center={props.center}
+//   >
+//   <div>
+//     {props.markers.map(marker => (
+//       <Marker
+//         key={`geoloc-${marker.id}`}
+//         position={{lat: Number(marker.latitude), lng: Number(marker.longitude)}}
+//         defaultAnimation={2}
+//       />
+//     ))}
+//     <Polyline
+//       path={[{lat: 37.7835164, lng: -122.4091674}, {lat: 37.786161, lng: -122.4102922}]}
+//       defaultAnimation={2}
+//       geodesic= {true}
+//       strokeColor = {'#FF0000'}
+//        strokeOpacity = {1.0}
+//        strokeWeight = {2}
+//     />
+//   </div>
+//
+//   </GoogleMap>
+// ));
 
 const GoogleMapWrapper = withGoogleMap(props => (
   <GoogleMap
@@ -12,13 +41,16 @@ const GoogleMapWrapper = withGoogleMap(props => (
     defaultZoom={15}
     center={props.center}
   >
-    {props.markers.map(marker => (
-      <Marker
-        key={`geoloc-${marker.id}`}
-        position={{lat: Number(marker.latitude), lng: Number(marker.longitude)}}
+    <div>
+      <Polyline
+        path={props.polyLineData}
         defaultAnimation={2}
+        geodesic= {true}
+        strokeColor = {'#FF0000'}
+        strokeOpacity = {1.0}
+        strokeWeight = {2}
       />
-    ))}
+    </div>
   </GoogleMap>
 ));
 
@@ -27,10 +59,11 @@ export default class FindMyDogMap extends React.Component {
     super(props);
     this.state = {
       markers: [],
-      center: {lat: 0, lng: 0}
+      center: {lat: 0, lng: 0},
+      polyLineData: []
     };
     this.handleMapLoad = this.handleMapLoad.bind(this);
-    this.getGeolocations = this.getGeolocations.bind(this);    
+    this.getGeolocations = this.getGeolocations.bind(this);
   }
 
   componentDidMount() {
@@ -38,16 +71,22 @@ export default class FindMyDogMap extends React.Component {
   }
 
   handleMapLoad(map) {
+    console.log('map is ', map);
     this._mapComponent = map;
   }
 
   getGeolocations() {
     $.get('/api/walks/track', {walkId: this.props.walkId})
       .then((data) => {
-        // console.log('SUCCESS getting geolocation ', data);
+        console.log('SUCCESS getting geolocation ', data);
+        var polydata = data.map(function(item, index) {
+          return {lat: Number(item.latitude), lng: Number(item.longitude)};
+        });
+        console.log('the poly data array is ', polydata);
         this.setState({
           markers: data,
-          center: data.length > 0 ? {lat: Number(data[0].latitude), lng: Number(data[0].longitude)} : {lat: 0, lng: 0}
+          center: data.length > 0 ? {lat: Number(data[0].latitude), lng: Number(data[0].longitude)} : {lat: 0, lng: 0},
+          polyLineData: polydata
         });
       })
       .fail((err) => {
@@ -68,6 +107,7 @@ export default class FindMyDogMap extends React.Component {
           onMapLoad={this.handleMapLoad}
           markers={this.state.markers}
           center={this.state.center}
+          polyLineData = {this.state.polyLineData}
         />
       </div>
     );
