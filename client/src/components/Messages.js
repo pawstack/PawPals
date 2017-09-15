91cf9c8e17b4511aee007f1aed66abfbc39b28cc
novyQ <novaqiu@gmail.com>
2017-09-15 15:18:43 -0700

    messages got ownerid and walkerid from owner link

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -75,6 +75,8 @@ class ChatList extends React.Component {
         conversationNames[conversationDetail.walker_id] = conversationDetail.walker;
 =======
     console.log('owner is',this.props.location.state.ownerid);
+    console.log('walker is',this.props.location.state.walkerid);
+
     this.fetchMessages()
     .then(() => {
       lastConvo = this.state.messages[this.state.messages.length - 1];
