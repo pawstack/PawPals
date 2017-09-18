9899efb1084ef0e4cc58d5eb7699b6068b3284a7
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-18 16:45:01 -0700

    add markers to map

--- client/src/components/StaticPastWalkMap.jsx
+++ client/src/components/StaticPastWalkMap.jsx
@@ -9,15 +9,12 @@ import {
   LatLng
 } from 'react-google-maps';
 
-
 class StaticPastWalkMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       markers: [],
       center: {lat: 0, lng: 0},
-      //polyLineData: [],
-      //zoom: 16,
       staticData: [],
       startLocation: {},
       endLocation: {}
@@ -29,7 +26,6 @@ class StaticPastWalkMap extends React.Component {
     this.getGeolocations();
   }
 
-
   getGeolocations() {
     var context = this;
     $.get('/api/walks/track', {walkId: this.props.walkId})
@@ -39,7 +35,6 @@ class StaticPastWalkMap extends React.Component {
           markers: data,
           center: data.length > 0 ? {lat: Number(data[midpoint].latitude), lng: Number(data[midpoint].longitude)} : {lat: 0, lng: 0},
         }, function() {
-
           var staticData = data.map(function(item, index) {
             return `${item.latitude},${item.longitude}`;
           });
@@ -58,29 +53,16 @@ class StaticPastWalkMap extends React.Component {
       });
 
   }
-
-
   render() {
     return (
       <div >
         <img
           style={{width: '100%', height: '400px'}}
           onClick ={this.props.updateAnimateState}
-          src = {`https://maps.googleapis.com/maps/api/staticmap?&zoom=16&path=color:black%7Cweight:5%7C${this.state.staticData}&markers=size:mid%7Ccolor:green%7Clabel:S%7C${this.state.startLocation}&markers=size:mid%7Ccolor:red%7Clabel:E%7C${this.state.endLocation}&scale=2&size=640x225&key=AIzaSyBvEUHNc1BSG-h4ZTcTxGZuBKjtFTrI9_8`}/>
+          src = {`https://maps.googleapis.com/maps/api/staticmap?&zoom=16&path=color:black%7Cweight:5%7C${this.state.staticData}&markers=size:mid%7Ccolor:green%7Clabel:A%7C${this.state.startLocation}&markers=size:mid%7Ccolor:red%7Clabel:B%7C${this.state.endLocation}&scale=2&size=640x225&key=AIzaSyA700R5j4-OD48-58Du-q2J-FK9DE52r-s`}/>
       </div>
     );
   }
 }
 
 export default StaticPastWalkMap;
-
-
-/*
-
-<div >
-  <img
-    style={{width: '100%', height: '400px'}}
-    onClick ={this.props.updateAnimateState}
-    src = {`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.center}&zoom=16&path=${this.state.staticData}&scale=2&size=640x225&key=AIzaSyBvEUHNc1BSG-h4ZTcTxGZuBKjtFTrI9_8`}/>
-</div>
-*/
