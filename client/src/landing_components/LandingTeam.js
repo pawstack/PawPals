c3da13edfa0d93b6704f2ed3e2077fdf342ae49b
novyQ <novaqiu@gmail.com>
2017-09-18 18:55:19 -0700

    formatted team page and added linkedin and github icons

--- client/src/landing_components/LandingTeam.js
+++ client/src/landing_components/LandingTeam.js
@@ -30,73 +30,56 @@ class LandingTeam extends React.Component {
 
           <div className="row team-container">
 
-            <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
-              <div className='icon-title'>Young Lu</div>
+            <div className="col-md-6">
+              <img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'>
+              </img>
+              <div className='icon-title'>
+                Young Lu
+                <span className='contact-icon-span'><a href="https://www.linkedin.com/in/yanglu3/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img></a></span>
+                <span className='contact-icon-span'><a href="https://github.com/youngyanglu"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img></a></span>
+              </div>
               <div className='icon-text'>
                 Calender system to easily schedule and book walks.
-              </div>
-              <div className="contact-icon-container">
-                  <div>
-                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
-                  </div>
-                  <div><br></br></div>
-                  <div>
-                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
-                  </div>
               </div>
             </div>
 
-            <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
-              <div className='icon-title'>Tiffany Choy</div>
+            <div className="col-md-6">
+              <img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'>
+              </img>
+              <div className='icon-title'>
+                Tiffany Choy
+                <span className='contact-icon-span'><a href="https://www.linkedin.com/in/tiffany-choy/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
+                <span className='contact-icon-span'><a href="https://github.com/tiffanycchoy"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
+              </div>
               <div className='icon-text'>
                 Calender system to easily schedule and book walks.
-              </div>
-              <div className="contact-icon-container">
-                  <div>
-                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
-                  </div>
-                  <div><br></br></div>
-                  <div>
-                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
-                  </div>
               </div>
             </div>
 
           </div>
 
+
           <div className="row team-container">
 
             <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
-              <div className='icon-title'>Martin Chang</div>
+              <div className='icon-title'>
+                Martin Chang
+                <span className='contact-icon-span'><a href="https://www.linkedin.com/in/martinkchang/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
+                <span className='contact-icon-span'><a href="https://github.com/mkchang"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
+              </div>
               <div className='icon-text'>
                 Calender system to easily schedule and book walks.
-              </div>
-              <div className="contact-icon-container">
-                  <div>
-                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
-                    <span><a href="https://github.com/mkchang">https://github.com/mkchang</a></span>
-                  </div>
-                  <div><br></br></div>
-                  <div>
-                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
-                      <span><a href="https://www.linkedin.com/in/martinkchang">https://www.linkedin.com/in/martinkchang/</a></span>
-                  </div>
               </div>
             </div>
 
             <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
-              <div className='icon-title'>Nova Qiu</div>
+              <div className='icon-title'>
+                Nova Qiu
+                <span className='contact-icon-span'><a href="https://www.linkedin.com/in/noqiu/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
+                <span className='contact-icon-span'><a href="https://github.com/novyQ"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
+              </div>
               <div className='icon-text'>
                 Calender system to easily schedule and book walks.
-              </div>
-              <div className="contact-icon-container">
-                  <div>
-                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
-                  </div>
-                  <div><br></br></div>
-                  <div>
-                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
-                  </div>
               </div>
             </div>
           </div>
