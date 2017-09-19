818af9bc864338b36b1091967f7f44e128c0f23a
novyQ <novaqiu@gmail.com>
2017-09-18 17:27:43 -0700

    added team page and styling with team page

--- client/src/landing_components/LandingTeam.js
+++ client/src/landing_components/LandingTeam.js
@@ -38,6 +38,7 @@ class LandingTeam extends React.Component {
                 <span className='contact-icon-span'><a href="https://github.com/youngyanglu"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
               </div>
 
+
             <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
               <div className='icon-title'>Young Lu</div>
 
@@ -55,6 +56,15 @@ class LandingTeam extends React.Component {
               </div>
               <div className='icon-text'>
                 Calender system to easily schedule and book walks.
+              </div>
+              <div className="contact-icon-container">
+                  <div>
+                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
+                  </div>
+                  <div><br></br></div>
+                  <div>
+                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
+                  </div>
               </div>
             </div>
 
@@ -88,29 +98,61 @@ class LandingTeam extends React.Component {
           </div>
 
             <div className="col-md-6"><img src="https://image.flaticon.com/icons/svg/237/237066.svg" className='landing-icon'></img>
+
+            <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
+
               <div className='icon-title'>Tiffany Choy</div>
               <div className='icon-text'>
-                Make and receive payment securely through the app.
+                Calender system to easily schedule and book walks.
+              </div>
+              <div className="contact-icon-container">
+                  <div>
+                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
+                  </div>
+                  <div><br></br></div>
+                  <div>
+                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
+                  </div>
               </div>
             </div>
+
           </div>
+
           <div className="row team-container">
-            <div className="col-md-6"><img src="https://image.flaticon.com/icons/svg/235/235861.svg" className='landing-icon'></img>
+
+            <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
               <div className='icon-title'>Martin Chang</div>
               <div className='icon-text'>
-                Real time tracking of dog during the walk.
+                Calender system to easily schedule and book walks.
+              </div>
+              <div className="contact-icon-container">
+                  <div>
+                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
+                    <span><a href="https://github.com/mkchang">https://github.com/mkchang</a></span>
+                  </div>
+                  <div><br></br></div>
+                  <div>
+                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
+                      <span><a href="https://www.linkedin.com/in/martinkchang">https://www.linkedin.com/in/martinkchang/</a></span>
+                  </div>
               </div>
             </div>
 
-            <div className="col-md-6">
-              <img src="https://i.pinimg.com/originals/83/58/1c/83581c5e0cb7d83fee4a2e44083637a2.png" className='landing-icon'>
-              </img>
+            <div className="col-md-6"><img src="https://cdn0.iconfinder.com/data/icons/time-icons-rounded/110/Calendar-Time-512.png" className='landing-icon'></img>
               <div className='icon-title'>Nova Qiu</div>
               <div className='icon-text'>
-                Instant chat in app for users for smooth communication.
+                Calender system to easily schedule and book walks.
+              </div>
+              <div className="contact-icon-container">
+                  <div>
+                    <img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img"></img>
+                  </div>
+                  <div><br></br></div>
+                  <div>
+                    <img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img"></img>
+                  </div>
               </div>
             </div>
-
           </div>
 
         </div>
