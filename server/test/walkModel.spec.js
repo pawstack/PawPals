dea9b9817d3a3bfe244f9dd52e7b8d7bf5ce2f9c
Young <young@severus.local>
2017-09-20 14:31:56 -0700

    created skeleton files for testing

---
+++ server/test/walkModel.spec.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const model = require('../../db/models');
+const dbUtils = require('../../db/lib/utils.js');
+
+describe('Walk Model', function () {
+  // Deletes all tables, creates new tables, and seeds tables with test data
+  beforeEach(function (done) {
+    dbUtils.rollbackMigrate(done);
+  });
+
+  // Resets database back to original settings
+  afterEach(function (done) {
+    dbUtils.rollback(done);
+  });
+
+  it('Should be able to retrieve data', function (done) {
+    model.Walk.fetchAll()
+      .then(function (results) {
+        expect(results.length).to.equal(500);
+        expect(results.at(0).get('id')).to.equal(1);
+        done();
+      })
+      .catch(function (err) {
+        // If this expect statement is reached, there's an error.
+        done(err);
+      });
+  });
+});
