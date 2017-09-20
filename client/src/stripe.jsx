1ab4acc8e50d38461fa71c3760196779c9ed5e01
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-20 15:08:50 -0700

    in progress - updating stripe payment registration

---
+++ client/src/stripe.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import SignUpStart from './components/SignUpStart';
+import NavBarNewUserLoggedIn from '../src/components/NavBarNewUserLoggedIn.jsx';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#66BB6A',
+  },
+});
+
+class Stripe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+        <div>
+          <Router>
+            <MuiThemeProvider muiTheme={muiTheme}>
+              <div>
+                <AppBar
+                  title="PawPals"
+                  iconElementRight = {<NavBarNewUserLoggedIn />}
+                  showMenuIconButton={false}
+                />
+              <div><br></br><br></br></div>
+                <Route exact path="/signup/start" component={SignUpStart}/>
+              </div>
+            </MuiThemeProvider>
+          </Router>
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<Stripe />, document.getElementById('stripe-root'));
