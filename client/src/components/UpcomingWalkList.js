a9157119ac28bf1e79b544258653753ed7592ba8
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-20 21:21:06 -0700

    edit font size

--- client/src/components/UpcomingWalkList.js
+++ client/src/components/UpcomingWalkList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import UpcomingWalkItem from './UpcomingWalkItem';
 import {Link} from 'react-router-dom';
+import '../../../public/componentCSS/upcoming-walk-list.css';
 
 const UpcomingWalkList = (props) => (
   <div>
     <h2>Upcoming Walks</h2>
-    {props.upcomingwalks.length > 0 ? 
+    {props.upcomingwalks.length > 0 ?
       props.upcomingwalks.map((walk) => (
         <UpcomingWalkItem key={`walk-${walk.id}`} walk={walk} cancelWalk={props.cancelWalk} />
       )) :
-      <p>No upcoming walks at this time. <Link to="/browse" >Browse for walks here!</Link></p>
+      <div className = 'no-upcomming-walks'>
+        No upcoming walks at this time.
+        <br></br>
+        <Link to="/browse" >Browse for walks here!
+        </Link>
+      </div>
     }
   </div>
 );
 
 export default UpcomingWalkList;
-
