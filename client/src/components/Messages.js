c5c4f652686a126c3219040b705f649c54b32150
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-09-21 20:30:00 -0700

    debug message count

--- client/src/components/Messages.js
+++ client/src/components/Messages.js
@@ -5,32 +5,37 @@ import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 import ChatWindow from './ChatWindow';
-import Notification  from 'react-web-notification';
+import Notification from 'react-web-notification';
 import $ from 'jquery';
-import '../../../public/componentCSS/messages.css'
+import Badge from 'material-ui/Badge';
+import '../../../public/componentCSS/messages.css';
 
 class ChatList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       owner: null,
       conversationNames: {},
       user_id: null,
-      chat: [] ,
+      chat: [],
       ignore: true,
-      title: ''
-    }
+      title: '',
+      newMessageCount: 0
+    };
     this.fetchMessages = this.fetchMessages.bind(this);
     this.instantiateConversation = this.instantiateConversation.bind(this);
     this.getConversationDetails = this.getConversationDetails.bind(this);
     this.generateConversationNames = this.generateConversationNames.bind(this);
     this.checkNewConversation = this.checkNewConversation.bind(this);
+    //this.fetchNewMessageCount = this.fetchNewMessageCount.bind(this);
   }
 
   componentDidMount() {
     this.props.socket.on('new message', (data) => {
       console.log('new message heard')
+      console.log('$$$$$$$$$the data is', data);
       this.handleNewMessage.call(this, data);
+      //this.fetchNewMessageCount(data);
     })
     this.getConversationDetails((conversationDetails) => {
       this.generateConversationNames(conversationDetails, () => {
@@ -66,14 +71,24 @@ class ChatList extends React.Component {
     for (var i = 0; i < conversationDetails.details.length; i++) {
       var conversationDetail = conversationDetails.details[i];
       if (this.state.owner) {
-        conversationNames[conversationDetail.walker_id] = conversationDetail.walker;
+        if (!conversationNames[conversationDetail.walker_id]) {
+          conversationNames[conversationDetail.walker_id] = conversationDetail.walker;
+        }
         if (conversationDetail.owner_read) {
           conversationNames[conversationDetail.walker_id]['read'] = true;
+        } else {
+          conversationNames[conversationDetail.walker_id]['count'] = conversationNames[conversationDetail.walker_id]['count'] || 0;
+          conversationNames[conversationDetail.walker_id]['count']++;
         }
       } else {
-        conversationNames[conversationDetail.owner_id] = conversationDetail.owner;
+        if (!conversationNames[conversationDetail.owner_id]) {
+          conversationNames[conversationDetail.owner_id] = conversationDetail.owner;
+        }
         if (conversationDetail.walker_read) {
           conversationNames[conversationDetail.owner_id]['read'] = true;
+        } else {
+          conversationNames[conversationDetail.owner_id]['count'] = conversationNames[conversationDetail.owner_id]['count'] || 0;
+          conversationNames[conversationDetail.owner_id]['count']++;
         }
       }
     }
@@ -127,6 +142,7 @@ class ChatList extends React.Component {
   fetchMessages(other_person_id) {
     var conversationNames = this.state.conversationNames;
     conversationNames[other_person_id].read = true;
+    conversationNames[other_person_id].count = 0;
     this.setState({conversationNames});
     return fetch('/api/messages/fetch', {
       method: 'POST',
@@ -191,22 +207,78 @@ class ChatList extends React.Component {
     }
   }
 
+  // fetchNewMessageCount () {
+  //   console.log('**** INSIDE OF FETCH MESSAGE COUNT');
+  //   var context = this;
+  //   $.ajax({
+  //     method: 'GET',
+  //     url: '/api/messages/fetchNewCount',
+  //     // data: {
+  //     //   walker_id:
+  //     //   owner_id: this.state.owner
+  //     // },
+  //
+  //     success(success_data) {
+  //       //console.log('the NEW MSG success_data fetched from db is ', success_data);
+  //       var receiver = '';
+  //       var count = 0;
+  //       console.log('data id is ', context.state.user_id);
+  //
+  //       var id = Number(Object.keys(context.state.conversationNames));
+  //       console.log('#####the id is ', id);
+  //
+  //       console.log('**conversation names is ', context.state.conversationNames[id].owner);
+  //       context.state.conversationNames.owner ? receiver = 'walker' : receiver = 'owner';
+  //       for (var i = 0; i < success_data.length; i++) {
+  //         console.log('success_data i is ', success_data[i]);
+  //         console.log('success_data identity read is ', success_data[i][receiver + '_read']);
+  //         if (success_data[i][receiver + '_read'] === false) {
+  //           //increase the new message count.
+  //           count += 1;
+  //         }
+  //       }
+  //       console.log('receiver is ', receiver);
+  //       console.log('*****the number of unread messages is ', count);
+  //       context.setState({
+  //         newMessageCount: count
+  //       });
+  //     },
+  //     error(err) {
+  //       console.log('ERROR - NEW MSG success_data fetched from db is ', err);
+  //     }
+  //   });
+  // }
+
   render() {
     if (this.state.owner) {
       var other_person = 'walker';
     } else {
       var other_person = 'owner';
     }
+    console.log('*****conversaion names is ', this.state.conversationNames);
+
+
    return (
      <div>
        <List>
          <div className = 'chat-header'>Chats</div>
          {(Object.keys(this.state.conversationNames)).map(key => (
+
            <ListItem
              style={{"fontWeight": this.state.conversationNames[key].read? "normal": "bold"}}
              primaryText={this.state.conversationNames[key].first + ' ' + this.state.conversationNames[key].last}
              leftAvatar={<Avatar src= {this.state.conversationNames[key].profile_pic} />}
-             rightIcon={<CommunicationChatBubble />}
+             rightIcon={
+               !this.state.conversationNames[key].read ? (
+               <Badge
+                 badgeContent={this.state.conversationNames[key].count}
+                 primary={true}
+               >
+                 <CommunicationChatBubble />
+               </Badge>) : (
+               <CommunicationChatBubble />
+)
+             }
              onClick={() => {this.fetchMessages(key)}}
            />
          ))}
