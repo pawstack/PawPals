24164a9612ec865797932cb44a9596c1186fd5a9
novyQ <novaqiu@gmail.com>
2017-09-25 18:26:29 -0700

    Added team quotes

--- client/src/landing_components/LandingTeam.js
+++ client/src/landing_components/LandingTeam.js
@@ -21,8 +21,8 @@ class LandingTeam extends React.Component {
                 <span className='contact-icon-span'><a href="https://www.linkedin.com/in/yanglu3/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
                 <span className='contact-icon-span'><a href="https://github.com/youngyanglu"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
               </div>
-              <div className='icon-text'>
-
+              <div className='team-text'>
+                Economist turned software engineer looking for inefficiencies to fix.
               </div>
             </div>
 
@@ -34,8 +34,8 @@ class LandingTeam extends React.Component {
                 <span className='contact-icon-span'><a href="https://www.linkedin.com/in/tiffany-choy/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
                 <span className='contact-icon-span'><a href="https://github.com/tiffanycchoy"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
               </div>
-              <div className='icon-text'>
-
+              <div className='team-text'>
+                Passionate about technology that connects people in their every day lives.
               </div>
             </div>
 
@@ -51,8 +51,8 @@ class LandingTeam extends React.Component {
                 <span className='contact-icon-span'><a href="https://www.linkedin.com/in/martinkchang/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
                 <span className='contact-icon-span'><a href="https://github.com/mkchang"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
               </div>
-              <div className='icon-text'>
-
+              <div className='team-text'>
+                Energy enthusiast, excited to build technologies that leverage data in interesting ways.
               </div>
             </div>
 
@@ -62,14 +62,16 @@ class LandingTeam extends React.Component {
                 <span className='contact-icon-span'><a href="https://www.linkedin.com/in/noqiu/"><img src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/linkedin-256.png" className="contact-icon-img" alt="linkedin"></img></a></span>
                 <span className='contact-icon-span'><a href="https://github.com/novyQ"><img src="https://mapbox.github.io/mapbox-gl-native/macos/0.5.0/img/github.svg" className="contact-icon-img" alt="github"></img></a></span>
               </div>
-              <div className='icon-text'>
-
+              <div className='team-text'>
+                Loves creating interaction and user experience, and bringing design to life.
               </div>
             </div>
           </div>
 
         </div>
+
     );
+
   }
 
 }
